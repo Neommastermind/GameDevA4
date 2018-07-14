55b3f798f3b40826b56b72ec6bd705c6931cd1ab
Neommastermind <31413602+Neommastermind@users.noreply.github.com>
2018-07-14 00:11:35 -0600

    All features complete
    All of the features are done, but it exhibits some strange behavior occasionally.

--- A4/Assets/Scripts/Bullet.cs
+++ A4/Assets/Scripts/Bullet.cs
@@ -6,6 +6,7 @@ public class Bullet : MonoBehaviour {
 
 	public float speed;
 	private Rigidbody rb;
+	private float collisionTime = 0.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -22,10 +23,28 @@ public class Bullet : MonoBehaviour {
 	}
 
 	void OnCollisionEnter(Collision collision) {
-		if(collision.gameObject.CompareTag("Target")) {
+		if (collision.gameObject.CompareTag ("Special") && Time.time > collisionTime + 0.25) {
+			collisionTime = Time.time;
 			collision.gameObject.GetComponent<AudioSource> ().Play ();
-		}
+			collision.gameObject.GetComponent<ParticleSystem> ().Play ();
+			GameObject.FindGameObjectWithTag ("Player").GetComponent<Ammo> ().AddAmmo (AmmoEquipper.activeAmmoType, 10);
+			/*
+			float xComponent = rb.velocity.x != 0 ? rb.velocity.x / Mathf.Abs (rb.velocity.x) : 0;
+			float yComponent = rb.velocity.y != 0 ? rb.velocity.y / Mathf.Abs (rb.velocity.y) : 0;
+			float zComponent = rb.velocity.z != 0 ? rb.velocity.z / Mathf.Abs (rb.velocity.z) : 0;
+			rb.AddForce (100 * new Vector3 (xComponent, -yComponent, -zComponent));*/
+			Vector3 reflected = Vector3.Reflect (rb.velocity, collision.contacts [0].normal);
+			//transform.rotation = Quaternion.FromToRotation (rb.velocity, reflected) * transform.rotation;
+			rb.velocity = reflected;
 
-		Destroy (gameObject);
+		}
+		else if (collision.gameObject.CompareTag ("Target")) {
+			collision.gameObject.GetComponent<AudioSource> ().Play ();
+			collision.gameObject.GetComponentInParent<ParticleSystem> ().Play ();
+			Destroy (gameObject);
+		}
+		else if(!collision.gameObject.CompareTag("Boundary")) {
+			Destroy (gameObject);
+		}
 	}
 }
